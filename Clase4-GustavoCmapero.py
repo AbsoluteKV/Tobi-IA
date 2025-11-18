6e7a4c0624178694a71c85198974695f12d412a2
AbsoluteKV <gustavoc4797@gmail.com>
2025-11-17 23:47:29 -0300

    Add files via upload

---
+++ Clase4-GustavoCmapero.py
@@ -0,0 +1,12 @@
+nro = int(input("ingresar número: "))
+
+def marcar_pares():
+ for i in range(nro+ 1):
+        if i % 2 == 0:
+            print("es par", i)
+        else:
+            print(i)
+      
+marcar_pares()
+
+
